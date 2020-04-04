26c8a03ffcd2d51a18429b712b1e4f861651ac87
yankur <yanusyakurlyak@gmail.com>
2020-04-04 21:27:37 +0300

    Commit

---
+++ cmake-build-debug/CMakeFiles/lab4_word_count.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4_word_count.dir/count_words.cpp.o"
+  "lab4_word_count"
+  "lab4_word_count.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4_word_count.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ run.py
@@ -0,0 +1,46 @@
+import sys
+import subprocess
+import matplotlib.pyplot as plt
+
+
+def main(n):
+    min_time = 1000000
+    time = []
+    for threads in range(n):
+        generate_config(threads)
+        out = subprocess.Popen(['./count', 'config.dat'],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+        stdout, stderr = out.communicate()
+        min_time = min(int(stdout.split()[-1]), min_time)
+        time.append(int(stdout.split()[-1]))
+    generate_graph(n, time)
+    print('Minimal time: ', min_time)
+
+
+def test(threads):
+    generate_config(threads)
+    out = subprocess.Popen(['./count', 'config.dat'],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT)
+    stdout, stderr = out.communicate()
+
+
+def generate_config(threads):
+    conf_file = open('config.dat', 'w')
+    conf_file.write('infile=\"data.txt\"\nout_by_a=\"res_a.txt\"\nout_by_n=\"res_n.txt\"\nthreads={}'.format(threads))
+    conf_file.close()
+
+
+def generate_graph(threads, time):
+    threads = [_ for _ in range(1, threads + 1)]
+    plt.plot(threads, time)
+    plt.xlabel('n of threads')
+    plt.ylabel('time ms')
+    plt.savefig('time_vs_threads_graph.png')
+
+
+if sys.argv[1] != 'test':
+    main(int(sys.argv[1]))
+else:
+    test(sys.argv[2])
