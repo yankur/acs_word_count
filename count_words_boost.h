b14e450b050559ab564a3a8c06447193e0ddb6fb
yankur <42523950+yankur@users.noreply.github.com>
2020-04-05 18:32:18 +0300

    Update count_words_boost.h

--- count_words_boost.h
+++ count_words_boost.h
@@ -1,11 +1,12 @@
 #ifndef LAB4_WORD_COUNT_COUNT_WORDS_H
 #define LAB4_WORD_COUNT_COUNT_WORDS_H
 
-#endif //LAB4_WORD_COUNT_COUNT_WORDS_H
-
 #include <ctype.h>
 #include <string>
 #include <map>
 #include <bits/unordered_map.h>
+#include "my_concurrent_queue.h"
 
-std::unordered_map<std::string, size_t> count_words(const std::string inp_string);
+void count_words(const std::string &inp_string, ConcurrentQueue<std::unordered_map<std::string, size_t>> &queue);
+
+#endif //LAB4_WORD_COUNT_COUNT_WORDS_H
